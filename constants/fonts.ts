cb6a39b0925841668d774637dd0f1b368edeff20
munnakumar678997 <munnakumar678997@gmail.com>
2026-07-05 18:16:12 +0530

    Add constants/fonts.ts

---
+++ constants/fonts.ts
@@ -0,0 +1,12 @@
+export const FONTS = {
+  regular: 'System',
+  bold: 'System',
+  sizes: {
+    xs: 11,
+    small: 13,
+    medium: 16,
+    large: 20,
+    xlarge: 26,
+    xxlarge: 34,
+  },
+};
